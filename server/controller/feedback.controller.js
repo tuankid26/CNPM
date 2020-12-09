6b4e1e3b167f5ca93921276e575e5594c2cac725
tuankid26 <minhtuantna01@gmail.com>
2020-12-09 17:47:34 +0700

    Toang

--- server/controller/feedback.controller.js
+++ server/controller/feedback.controller.js
@@ -2,7 +2,7 @@ const { json } = require('express');
 var db = require('../db');
 
 module.exports.getAllFeedbacks = function(req, res){
-    db.executeQuery('SELECT * FROM FEEDBACK WHERE ID IN (SELECT ID_FEEDBACK IN Mapping)', function(data, err){
+    db.executeQuery('SELECT * FROM FEEDBACK', function(data, err){
         if (err){
             // res.writeHead(500, 'Internal error', {'Content-type': 'text/html'})
             res.send('error occured');
@@ -23,32 +23,13 @@ module.exports.getAllFeedbacks = function(req, res){
 };
 
 module.exports.addFeedback = function(req, res){
-    var noidung = req.body.noidung;
+    var name = req.body.name;
     var time = req.body.time;
     var status = req.body.status;
-    var hoten =  req.body.hoten;
-    var diachi = req.body.diachi;
-    var ngaysinh = req.body.ngaysinh;
-    var gioitinh =  req.body.gioitinh;
-    db.executeQuery('SELECT hoten FROM NHANKHAU WHERE hoten =  ' + hoten, function(err, data){
-        if (data.length == 0){
-            db.executeQuery('INSERT INTO NHANKHAU VALUES (' + hoten + ', ' + ngaysinh + ', ' + diachi + ', ' + gioitinh + ')');
-        }
-    }
-    )
-
-    db.executeQuery('INSERT INTO FEEDBACK VALUES (' + noidung + ', ' + time + ', ' + status + ')', function(err,data){
+    db.executeQuery('INSERT INTO FEEDBACK VALUES (' + name + ', ' + time + ', ' + status + ')', function(err){
         res.writeHead(500);
         res.send(JSON.stringify(err));
     });
-
-    db.executeQuery('INSERT INTO MAPPING VALUES ( SELECT ID_FEEDBACK FROM FEEDBACK WHERE FEEDBACK.noiDung = ' + noidung +', SELECT ID_NHANKHAU FROM NHANKHAU WHERE NHANKHAU.Hoten = ' + hoten + ', ' + time + ')', function(err,data){
-        res.writeHead(500);
-        res.send(JSON.stringify(err));
-    });
-
-
-    
 
 };
 
@@ -61,6 +42,7 @@ module.exports.findFeedback = function(req, res){
         }
         else{
             res.send(data);
+            
         }
     });
 };
@@ -83,15 +65,3 @@ module.exports.deleteFeedback = function(req, res){
         res.send(err);
     });
 };
-
-
-module.exports.mergeFeedback = function(req, res){
-    var list_idMerge = req.params.list_idMerge;
-    
-    // for (i = 0; i < list_idMerge.length; i++){
-    //     db.executeQuery('SELECT ID_NHANHKHAU, ID_FEEDBACK FROM Mapping WHERE')
-        
-    // }
-    db.executeQuery('UPDATE Mapping SET ID_FEEDBACK =' + list_idMerge[0] + 'WHERE ID_FEEDBACK IN' + list_idMerge + ')';
-
-}
